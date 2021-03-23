a38e1c583b3a363fb4eeac3cb9d17a42cb17ca85
AtwanZ <33740557+AtwanZ@users.noreply.github.com>
2021-03-22 23:59:31 -0400

    pushing assests

---
+++ Assets/Cards/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardElement // creates a collection of related constants
+{
+    Earth,
+    Fire,
+    Water,
+    Mind,
+    Matter,
+    Soul,
+    Light,
+    Dark,
+    Space,
+    Aether,
+    Plasma
+}
+
+public enum CardType
+{
+    Ruler,
+    Minion,
+    Spell
+}
+
+[System.Serializable] // Lets Unity know that this class can be serialized
+public class Card
+{
+    public int cardId;
+    public string cardName;
+    public CardElement cardElement;
+    public CardType cardType;
+    public int cost;
+}
---
+++ Assets/Cards/UIManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public CardManager cardManager;
+    public GameObject[] cardSlots;
+
+    private void Start()
+    {
+        DisplayCards();
+    }
+
+    private void DisplayCards()
+    {
+        for(int i = 0; i < cardManager.cards.Count; i++)
+        {
+            // Assign the cardManager's card information to the UI
+            cardSlots[i].transform.GetChild(1).transform.GetChild(0).GetComponent<Text>().text = cardManager.cards[i].cardName;
+
+            /*
+            switch (cardManager.cards[i].cardElement) // Switch statements provide a way to test equality against a list of values
+            {
+                case CardElement.Earth:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Earth";
+                        break;
+                    }
+                case CardElement.Fire:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Fire";
+                        break;
+                    }
+                case CardElement.Water:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Water";
+                        break;
+                    }
+                case CardElement.Mind:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Mind";
+                        break;
+                    }
+                case CardElement.Matter:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Matter";
+                        break;
+                    }
+                case CardElement.Soul:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Soul";
+                        break;
+                    }
+                case CardElement.Light:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Light";
+                        break;
+                    }
+                case CardElement.Dark:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Dark";
+                        break;
+                    }
+                case CardElement.Space:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Space";
+                        break;
+                    }
+                case CardElement.Aether:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Aether";
+                        break;
+                    }
+                case CardElement.Plasma:
+                    {
+                        cardSlots[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Plasma";
+                        break;
+                    }
+            }
+            */
+        }
+    }
+}
